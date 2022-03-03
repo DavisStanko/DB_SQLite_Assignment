ea7a51a705252ef4f2966d042b9da558f7d1b993
davisstanko <davisbvstanko@gmail.com>
2022-03-03 17:11:58 -0500

    Clean up

---
+++ main.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+import backend
+import sys
+
+# Naming color hexcodes
+# Taken from Gruvbox Dark theme
+bg0 = "#282828"
+bg1 = "#3c3836"
+bg2 = "#504945"
+bg3 = "#665c54"
+fg1 = "#ebdbb2"
+gruvYellow = "#d79921"  # Called gruv yellow as yellow is already a system color
+
+
+window = tk.Tk()
+window.title("Davis\' Coffee Shop Database")
+window.geometry("800x600")
+window.tk.call('tk', 'scaling', 2.0)  # Makes all widgets 2x as big.
+
+window.configure(background=bg1)  # changes background color
+
+# Product table menu
+createButton = tk.Button(text="(Re)Create Product Table", fg=fg1, bg=gruvYellow, highlightthickness="0", command=backend.create_product_table_UI).pack(anchor='n')
+addButton = tk.Button(text="Add new product", fg=fg1, bg=gruvYellow,  highlightthickness="0", command=backend.insert_UI).pack()
+updateButton = tk.Button(text="Update existing product", fg=fg1, bg=gruvYellow, highlightthickness="0", command=backend.update_UI).pack()
+deleteButton = tk.Button(text="Delete existing product", fg=fg1, bg=gruvYellow, highlightthickness="0", command=backend.delete_UI).pack()
+findButton = tk.Button(text="Find products", fg=fg1, bg=gruvYellow, highlightthickness="0", command=backend.select_products_UI).pack()
+listButton = tk.Button(text="List products", fg=fg1, bg=gruvYellow, highlightthickness="0", command=backend.list_products_UI).pack()
+exitButton = tk.Button(text="Exit", fg=fg1, bg=gruvYellow, highlightthickness="0", command=sys.exit).pack()
+
+window.mainloop()
