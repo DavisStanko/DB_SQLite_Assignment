0dc32f900bc0d8faaffa42897776fc8322978c8f
davisstanko <davisbvstanko@gmail.com>
2022-03-03 13:40:14 -0500

    ui started

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import backend
+import sys
 
 # Naming color hexcodes
 # Taken from Gruvbox Dark theme
@@ -19,9 +20,30 @@ window.tk.call('tk', 'scaling', 2.0)  # Makes all widgets 2x as big.
 window.configure(background=bg1)  # changes background color
 
 # Create the widgets
-prompt = tk.Label(foreground=fg1, background=bg1, text="")
+# Product table menu
+createButton = tk.Button(text="(Re)Create Product Table", fg=fg1, bg=gruvYellow,
+                         highlightthickness="0", command=backend.create_product_table_UI)
+addButton = tk.Button(text="Add new product", fg=fg1, bg=gruvYellow,
+                      highlightthickness="0", command=backend.insert_UI)
+updateButton = tk.Button(text="Update existing product", fg=fg1,
+                         bg=gruvYellow, highlightthickness="0", command=backend.update_UI)
+deleteButton = tk.Button(text="Delete existing product", fg=fg1,
+                         bg=gruvYellow, highlightthickness="0", command=backend.delete_UI)
+findButton = tk.Button(text="Find products", fg=fg1, bg=gruvYellow,
+                       highlightthickness="0", command=backend.select_products_UI)
+listButton = tk.Button(text="List products", fg=fg1, bg=gruvYellow,
+                       highlightthickness="0", command=backend.list_products_UI)
+exitButton = tk.Button(text="Exit", fg=fg1, bg=gruvYellow,
+                       highlightthickness="0", command=sys.exit)
 
 # Place the widgets
 prompt.pack(anchor="n")  # Anchor north (top middle)
+createButton.pack()
+addButton.pack()
+updateButton.pack()
+deleteButton.pack()
+findButton.pack()
+listButton.pack()
+exitButton.pack()
 
 window.mainloop()
