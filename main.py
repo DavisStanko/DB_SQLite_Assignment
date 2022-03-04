b04979c0f4682ec821de6216f565e87435f59dff
davisstanko <davisbvstanko@gmail.com>
2022-03-03 19:56:32 -0500

    Login screen!!!

--- main.py
+++ main.py
@@ -11,21 +11,41 @@ bg3 = "#665c54"
 fg1 = "#ebdbb2"
 gruvYellow = "#d79921"  # Called gruv yellow as yellow is already a system color
 
+def product_table():
+    # Product table menu
+    createButton = tk.Button(text="(Re)Create Product Table", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=backend.create_product_table_UI).pack()
+    addButton = tk.Button(text="Add new product", fg=fg1, bg=gruvYellow,  highlightthickness="0", borderwidth="0", command=backend.insert_UI).pack()
+    updateButton = tk.Button(text="Update existing product", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=backend.update_UI).pack()
+    deleteButton = tk.Button(text="Delete existing product", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=backend.delete_UI).pack()
+    findButton = tk.Button(text="Find products", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=backend.select_products_UI).pack()
+    listButton = tk.Button(text="List products", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=backend.list_products_UI).pack()
+    exitButton = tk.Button(text="Exit", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=sys.exit).pack()
 
-window = tk.Tk()
-window.title("Davis\' Coffee Shop Database")
-window.geometry("800x600")
-window.tk.call('tk', 'scaling', 2.0)  # Makes all widgets 2x as big.
+def login():
+    global loginPassword
+    inp = loginPassword.get()
+    if inp != "password":
+        return
+    else:
+        loginWindow.destroy()
+        window = tk.Tk()
+        window.title("Davis\' Coffee Shop Database")
+        window.geometry("800x600")
+        window.tk.call('tk', 'scaling', 2.0)  # Makes all widgets 2x as big.
+        window.configure(background=bg1)
+        product_table()
 
-window.configure(background=bg1)  # changes background color
+#FIRST WINDOW
+loginWindow = tk.Tk()
+loginWindow.title("Davis\' Coffee Shop Database")
+loginWindow.geometry("800x600")
+loginWindow.tk.call('tk', 'scaling', 2.0)  # Makes all widgets 2x as big.
+loginWindow.configure(background=bg1)  # Changes background color
 
-# Product table menu
-createButton = tk.Button(text="(Re)Create Product Table", fg=fg1, bg=gruvYellow, highlightthickness="0", command=backend.create_product_table_UI).pack(anchor='n')
-addButton = tk.Button(text="Add new product", fg=fg1, bg=gruvYellow,  highlightthickness="0", command=backend.insert_UI).pack()
-updateButton = tk.Button(text="Update existing product", fg=fg1, bg=gruvYellow, highlightthickness="0", command=backend.update_UI).pack()
-deleteButton = tk.Button(text="Delete existing product", fg=fg1, bg=gruvYellow, highlightthickness="0", command=backend.delete_UI).pack()
-findButton = tk.Button(text="Find products", fg=fg1, bg=gruvYellow, highlightthickness="0", command=backend.select_products_UI).pack()
-listButton = tk.Button(text="List products", fg=fg1, bg=gruvYellow, highlightthickness="0", command=backend.list_products_UI).pack()
-exitButton = tk.Button(text="Exit", fg=fg1, bg=gruvYellow, highlightthickness="0", command=sys.exit).pack()
+loginPassword = tk.Entry(fg=bg1, bg=fg1, highlightthickness="0", borderwidth="0")
+loginButton = tk.Button(text="Login", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=login)
 
-window.mainloop()
+loginPassword.pack()
+loginButton.pack()
+
+loginWindow.mainloop()
