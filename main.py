4dd7d982a6d8d2f5b3cf814cb336adbc7ef8736d
davisstanko <davisbvstanko@gmail.com>
2022-03-08 18:26:42 -0500

    Removed list button

--- main.py
+++ main.py
@@ -281,7 +281,7 @@ def delete_UI():
 def list_products_UI():
     def no():
         global window
-        listWindow.destroy()
+        findWindow.destroy()
         window = tk.Tk()
         window.title("Coffee Shop Database")
         window.geometry("800x600")
@@ -292,32 +292,16 @@ def list_products_UI():
             
     global window
     window.destroy()
-    listWindow = tk.Tk()
-    listWindow.title("Delete items from the Product Table")
-    listWindow.geometry("800x600")
-    listWindow.tk.call('tk', 'scaling', 0.75)  # Makes all widgets bigger.
-    listWindow.configure(background=bg1)
-    listWindow.grid_columnconfigure(0, weight=1)  # Makes the column stretch to fill the window.
-
-    my_tree = ttk.Treeview(listWindow, columns=("ProductID", "ProductName", "Cost"))
+    findWindow = tk.Tk()
+    findWindow.title("Delete items from the Product Table")
+    findWindow.geometry("800x600")
+    findWindow.tk.call('tk', 'scaling', 0.75)  # Makes all widgets bigger.
+    findWindow.configure(background=bg1)
+    findWindow.grid_columnconfigure(0, weight=1)  # Makes the column stretch to fill the window.
+  
+    noButton = tk.Button(findWindow, text="Cancel", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=no)
     
-    my_tree.heading("#0", text="", anchor=tk.W)
-    my_tree.heading("ProductID", text="ProductID", anchor=tk.W)
-    my_tree.heading("ProductName", text="ProductName", anchor=tk.W)
-    my_tree.heading("Cost", text="Cost", anchor=tk.W)
-    
-    my_tree.column("#0", width=0)
-    my_tree.column("ProductID", width=100)
-    my_tree.column("ProductName", width=100)
-    my_tree.column("Cost", width=100)
-    
-    my_tree.insert("", 0, text="ProductID", values=("ProductID", "ProductName", "Cost"))
-
-    noButton = tk.Button(listWindow, text="Cancel", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=no)
-    
-    my_tree.grid(row=0, column=0, padx=10, pady=10)
-    my_tree.grid_columnconfigure(0, weight=1)
-    noButton.grid(row=4, column=1, padx=10, pady=10)
+    noButton.grid(row=0, column=0, padx=10, pady=10)
     noButton.grid_columnconfigure(1, weight=1)
 
 def main_menu():
@@ -328,8 +312,7 @@ def main_menu():
     addButton = tk.Button(window, text="Add new product", fg=fg1, bg=gruvYellow,  highlightthickness="0", borderwidth="0", command=insert_UI)
     updateButton = tk.Button(window, text="Update existing product", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=update_UI)
     deleteButton = tk.Button(window, text="Delete existing product", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=delete_UI)
-    findButton = tk.Button(window, text="Find products", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=backend.select_products_UI)
-    listButton = tk.Button(window, text="List products", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=list_products_UI)
+    findButton = tk.Button(window, text="Find product", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=list_products_UI)
     exitButton = tk.Button(window, text="Exit", fg=fg1, bg=gruvYellow, highlightthickness="0", borderwidth="0", command=sys.exit)
 
     messageLabel.grid(row=0, column=0, padx=10, pady=10)
@@ -344,9 +327,7 @@ def main_menu():
     deleteButton.grid_columnconfigure(1, weight=1)
     findButton.grid(row=5, column=0, padx=10, pady=10)
     findButton.grid_columnconfigure(1, weight=1)
-    listButton.grid(row=6, column=0, padx=10, pady=10)
-    listButton.grid_columnconfigure(1, weight=1)
-    exitButton.grid(row=7, column=0, padx=10, pady=10)
+    exitButton.grid(row=6, column=0, padx=10, pady=10)
     exitButton.grid_columnconfigure(1, weight=1)
 
 def login():
